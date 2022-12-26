8bfe1d61bb69abf3dc6bb1ef5458978758145412
junior3874 <wellingtonjr53@Outlook.com.br>
2022-12-26 20:06:24 -0300

    refactor logic using dictionary in color and name translate

---
+++ src/utils/pokemon-type-dictionary.ts
@@ -0,0 +1,82 @@
+export const pokemonTypeDictionary = {
+    "grass": {
+        name: "Planta",
+        color: "#46A487"
+    },
+    "normal": {
+        name: "Comum",
+        color: "#eaeaea"
+    },
+    "fighting": {
+        name: "Lutador",
+        color: "#D56723"
+    },
+    "flying": {
+        name: "Voador",
+        color: "#F2F2F2"
+    },
+    "poison": {
+        name: "Veneno",
+        color: "#B97FC9"
+    },
+    "ground": {
+        name: "Terreste",
+        color: "#b59475"
+    },
+    "rock": {
+        name: "Pedra",
+        color: "#A38C21"
+    },
+    "bug": {
+        name: "Inseto",
+        color: "#668F38"
+    },
+    "ghost": {
+        name: "Fantasma",
+        color: "#B97FC9"
+    },
+    "stell": {
+        name: "Ferro",
+        color: "#9EB7B8"
+    },
+    "fire": {
+        name: "Fogo",
+        color: "#FB6C6C"
+    },
+    "water": {
+        name: "Água",
+        color: "#46C5D7"
+    },
+    "eletric": {
+        name: "Elétrico",
+        color: "#FFCE4B"
+    },
+    "psychic": {
+        name: "Psíquico",
+        color: "#DB5CA6"
+    },
+    "ice": {
+        name: "Gelo",
+        color: "#30A7D7"
+    },
+    "dragon": {
+        name: "Dragão",
+        color: "#F16E57"
+    },
+    "dark": {
+        name: "Noturno",
+        color: "#B4B4B4"
+    },
+    "fairy": {
+        name: "Fada",
+        color: "#FDB9E9"
+    },
+    "unknown": {
+        name: "Desconhecido",
+        color: "#B4B4B4"
+    },
+    "shadow": {
+        name: "Sombra",
+        color: "#B97FC9"
+    }
+}
