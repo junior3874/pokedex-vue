ab3d9552d9aeea52ddbc7543fff0f8723b38aec6
junior3874 <wellingtonjr53@Outlook.com.br>
2022-12-26 22:45:16 -0300

    refacor basic logic in component

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,15 +1,21 @@
 <template>
 
     <div class="divBusca">
-        <input v-model='search' type="text" id="txtBusca" placeholder="Buscar..." @change="current_Data()" />
+        <input v-model='search' type="text" id="txtBusca" placeholder="Buscar..." @change="current_Data" />
         <img src="../assets/search3.png.png" id="btnBusca" alt="Buscar" />
     </div>
 
     <div class="container">
-        <PokemonCard v-if="search == ''" v-for="(pokemon) in pkm" :name="pokemon.name" :url="pokemon.url"
-            v-bind:key="pokemon.name" />
-        <PokemonCard v-if="search" v-for="(pokemon) in pkmFiltredList" :name="pokemon.name" :url="pokemon.url"
-            v-bind:key="pokemon.name" />
+
+        <div class="pokemon-card-wrapper" v-if="!search">
+            <PokemonCard v-for="(pokemon) in pkm" :name="pokemon.name" :url="pokemon.url" v-bind:key="pokemon.name" />
+        </div>
+
+        <div v-else class="pokemon-card-wrapper">
+            <PokemonCard v-for="(pokemon) in pkmFiltredList" :name="pokemon.name" :url="pokemon.url"
+                v-bind:key="pokemon.name" />
+        </div>
+
     </div>
 
     <div class="buttons">
@@ -35,39 +41,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent } from 'vue';
 import api from '@/services/api';
 import PokemonCard from './PokemonCard.vue';
-import { pkmInterface } from '../interfaces/pkmInterface';
-
+type Pokemon = {
+    name: string;
+    url: string;
+}
 export default defineComponent({
     name: "Pokemon-vue",
     components: {
         PokemonCard
     },
+    data() {
+        return { pkm: [] as Pokemon[], offset: 0, page: 1, search: "", pkmCompleteList: [] as Pokemon[], pkmFiltredList: [] as Pokemon[] }
+    },
     beforeMount() {
         api.get(`/pokemon?limit=18&offset=${this.offset}`)
             .then(response => {
-
-                response.data.results.map((e: any) => {
-                    this.pkm.push(e)
-                })
+                this.pkm = response.data.results;
             });
-
-        api.get(`/pokemon?limit=151&offset=${this.offset}`)
-            .then(response => {
-
-                response.data.results.map((e: any) => {
-                    this.pkmCompleteList.push(e)
-                })
-            });
-
-
-    },
-
-
-    data() {
-        return { pkm: [] as pkmInterface, offset: 0, page: 1, search: "", pkmCompleteList: [] as pkmInterface, pkmFiltredList: [] as pkmInterface }
     },
     methods: {
         updatePagination(x: string) {
@@ -79,44 +72,37 @@ export default defineComponent({
                 this.offset += 18
                 this.page++
             }
-            if (this.page == 9) {
-                api.get(`/pokemon?limit=7&offset=${this.offset}`)
+            else {
+                const limit = this.page === 9 ? 7 : 18;
+                api.get(`/pokemon?limit=${limit}&offset=${this.offset}`)
                     .then(response => {
-
                         this.pkm = response.data.results
-
                     });
 
-            } else {
-                api.get(`/pokemon?limit=18&offset=${this.offset}`)
-                    .then(response => {
-
-                        this.pkm = response.data.results
-
-                    });
             }
 
         },
-        current_Data() {
-            return (
-                this.pkmCompleteList.filter((element: JSON) => {
-                    if (element.name.toLowerCase().includes(this.search.toLowerCase())){
-                        this.pkmFiltredList.push(element)
+        async current_Data() {
 
-                       
-                        //console.log(JSON.parse(JSON.stringify(element)))    
-                        
-                    }
+            if (this.pkmCompleteList.length === 0) {
+                const res =
+                    await api.get(`/pokemon?limit=151&offset=0`)
+                        .then(res =>
+                            res.data
+                        ).catch(err => ({
+                            error: true,
+                            message: err
+                        }));
 
+                if (res?.error) return
 
+                this.pkmCompleteList = res.results
+                const result = res.results.filter((element: Pokemon) => element.name.toLowerCase().includes(this.search.toLowerCase()));
+                return this.pkmFiltredList = result
+            }
 
-                    //console.log(element.name .toLowerCase() .includes(this.search.toLowerCase()))
-                        
-                        
-                }
-
-                ) || []
-            )
+            const result = this.pkmCompleteList.filter(element => element.name.toLowerCase().includes(this.search.toLowerCase()));
+            return this.pkmFiltredList = result
         }
 
     },
@@ -127,11 +113,21 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+.pokemons {
+    display: flex;
+    flex-direction: column;
+}
+
 .divBusca {
     margin-left: 16px;
     border: solid 1px;
     border-radius: 15px;
-    width: 300px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    width: 100%;
+    max-width: 300px;
 
     #txtBusca {
         float: left;
@@ -140,19 +136,20 @@ export default defineComponent({
         font-style: italic;
         font-size: 18px;
         border: none;
-        height: 32px;
-        width: 260px;
+        height: 100%;
+        width: 100%;
+        outline: none;
     }
 }
 
 
 .container {
-
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
-    grid-gap: 20px;
-    padding: 20px;
-
+    .pokemon-card-wrapper {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+        grid-gap: 20px;
+        padding: 20px;
+    }
 }
 
 .buttons {
